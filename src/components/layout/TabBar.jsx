424e4120a43f17a1c985643b4db137c061bbadea
DoculabsGlobal <clehman@doculabs.com>
2026-03-06 11:07:30 -0500

    growing issue

--- src/components/layout/TabBar.jsx
+++ src/components/layout/TabBar.jsx
@@ -37,7 +37,6 @@ export default function TabBar({ activeTab, onTabChange, validationUnlocked }) {
           <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11" />
         </svg>
       ),
-      disabled: !validationUnlocked,
     },
   ];
 
